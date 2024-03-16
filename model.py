ec3bbc9fda8a847ee42b1d2ffb2aaa1936f0c860
muyou <952349605@qq.com>
2024-03-16 17:07:10 +0800

    add comments

--- model.py
+++ model.py
@@ -107,7 +107,7 @@ class UpsampleBLock(nn.Module):
     def __init__(self, in_channels, up_scale):
         super(UpsampleBLock, self).__init__()
         self.conv = nn.Conv2d(in_channels, in_channels * up_scale ** 2, kernel_size=3, padding=1)
-        self.pixel_shuffle = nn.PixelShuffle(up_scale)
+        self.pixel_shuffle = nn.PixelShuffle(up_scale)  # (b,c*r*r,h,w)->(b,c,r*h,r*w) where r is an upscale factor
         self.prelu = nn.PReLU()
 
     def forward(self, x):
